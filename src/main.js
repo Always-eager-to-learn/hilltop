3daa509e3f394c9fb4ca2ee5962940a300099469
Always-eager-to-learn <avinodaaron06@gmail.com>
2025-08-29 12:49:39 +0530

    new changes

--- src/main.js
+++ src/main.js
@@ -4,18 +4,40 @@ import './styles/header.css'
 import './styles/main.css'
 import './styles/footer.css'
 import './styles/responsive.css'
+import { createIcons, icons } from 'lucide'
 
 const navigationBar = document.getElementById('mainNavigation')
 const openButton = document.querySelector('.mobile-nav')
 const closeButton = document.querySelector('.mobile-close-nav')
 const navigationIcons = document.querySelectorAll('.nav-item a')
-let location = 0
 const searchOpen = document.querySelector('.open-search')
 const searchClose = document.querySelector('.search-close')
 const searchDrawer = document.querySelector('.search-drawer')
+const sliderLeft = document.querySelector('.slider-left')
+const sliderRight = document.querySelector('.slider-right')
+const slider = document.querySelector('.slider')
+const sliderItems = document.querySelectorAll('.slider-image')
+const totSlides = sliderItems.length
+
+let currentLocation = Math.floor(Math.random() * 3)
+let location = 0
 
 function toggle(location = 0){
     navigationIcons[location].classList.toggle('active')
+}
+
+function setSvg(){
+    const svgL = document.createElement('i')
+    svgL.setAttribute('data-lucide', 'arrow-big-left-dash')
+    svgL.setAttribute('aria-hidden', true)
+    sliderLeft.append(svgL)
+
+    const svgR = document.createElement('i')
+    svgR.setAttribute('data-lucide', 'arrow-big-right-dash')
+    svgR.setAttribute('aria-hidden', true)
+    sliderRight.append(svgR)
+
+    createIcons({icons})
 }
 
 function toggleNavigation(element){
@@ -65,11 +87,43 @@ function searchBar(){
     })
 }
 
+function slideImages(){
+    
+    if(slider && sliderItems.length > 0){
+        const updateSlider = (left) => {
+            sliderItems[currentLocation].classList.remove('active')
+
+            if(!left){
+                currentLocation = (currentLocation + 1) % totSlides
+                sliderItems[currentLocation].classList.add('active')
+            }
+
+            else{
+                currentLocation = (currentLocation - 1)
+                if(currentLocation < 0)
+                    currentLocation = totSlides - 1
+                sliderItems[currentLocation].classList.add('active')
+            }
+        }
+
+        sliderLeft.addEventListener('click', () => {
+            updateSlider(true)
+        })
+
+        sliderRight.addEventListener('click', () => {
+            updateSlider(false)
+        })
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     navigationIcons[0].classList.toggle('active')
+    sliderItems[currentLocation].classList.toggle('active')
 
+    setSvg()
     navigationBarSettings()
     escapeKeyNavigation()
     navigationColor()
     searchBar()
+    slideImages()
 })
